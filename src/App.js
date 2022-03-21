6e4b304d26ab3831b6ab5888298cc8fc7f5226cd
Dasha Bataeva <dora.bataeva@gmail.com>
2022-03-21 15:15:57 -0400

    render random quote, basic styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import quotes from '/Users/coreycosman/random-quote/src/QuotesDatabase.js';
+import "./App.css";
+import React from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [quote, setQuote] = useState(quotes[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='quotebox'>
+       {quote.quote} 
+       <div>
+         by {quote.author}
+       </div>
+      <button onClick={()=> setQuote(quotes[Math.floor(Math.random()*10)])}>New Quote</button>
     </div>
   );
 }
